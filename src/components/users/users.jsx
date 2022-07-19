aa0ff1a2780e77e6f49f4fccef5707a31fd98f6a
Vlad <gadirovvladlen@gmail.com>
2022-07-19 13:26:15 +0300

    first for this app

---
+++ src/components/users/users.jsx
@@ -0,0 +1,9 @@
+import React from 'react'; //imr sfc
+
+const Users = () => {
+    return ( 
+        <h1>Users</h1>
+     );
+}
+ 
+export default Users;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Users from './components/users/users';
+import './index.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
